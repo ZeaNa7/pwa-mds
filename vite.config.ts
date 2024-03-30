25290f80ebd7d0dab4ee7f22b446a98d7eb5a154
Ana <anathrs72@gmail.com>
2024-03-30 19:23:25 +0100

    ♻️ (manifest): correction config file

--- vite.config.ts
+++ vite.config.ts
@@ -7,22 +7,35 @@ export default defineConfig({
   plugins: [react(),
     VitePWA({ 
       registerType: 'autoUpdate',
-      includeAssets: ['bird.png', 'vite.svg'],
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
-        name: 'Pwa Vite Project',
-        background_color: '#ffffff',
-        short_name: 'PWA',
+        name: 'Vite PWA Project',
+        short_name: 'Vite PWA Project',
         theme_color: '#ffffff',
         icons: [
             {
-                src: '/public/bird.png',
+                src: 'pwa-64x64.png',
                 sizes: '64x64',
+                type: 'image/png'
+            },
+            {
+                src: 'pwa-192x192.png',
+                sizes: '192x192',
+                type: 'image/png'
+            },
+            {
+                src: 'pwa-512x512.png',
+                sizes: '512x512',
                 type: 'image/png',
-                purpose: "any maskable"
+                purpose: 'any'
+            },
+            {
+                src: 'maskable-icon-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'maskable'
             }
         ],
-        start_url: '/',
-        scope: '/',
       }, 
     })
   ],
